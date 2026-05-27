0b87dd7f076e1bd99a755b910897e34cf0a400e2
Himanshu <hnannore01@gmail.com>
2026-05-28 01:52:21 +0530

    terraform file added

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+
 
 }
 resource "aws_instance" "myec2" {
   ami                    = "ami-0388e3ada3d9812da"
   instance_type          = "t3.micro"
   key_name               = "himanshu.key.pem"
-  vpc_security_group_ids = "mysg"
+  vpc_security_group_ids = ["mysg"]
   tags = {
     Name = "myec2"
     name = "Himanshu"
